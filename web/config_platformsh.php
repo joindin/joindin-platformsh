5d2090c08595c7fdf91e6b5267376525067f0300
Paul L. McNeely <paul@paulmcneely.com>
2019-02-28 14:26:46 -0600

    fixed service name added redis

--- web/config_platformsh.php
+++ web/config_platformsh.php
@@ -7,7 +7,8 @@ if (empty($routes)) {
     throw new \Exception("PLATFORM_ROUTES Not Set Or Empty.");
 }
 
-function getDomainFor(string $id) {
+function getDomainFor(string $id)
+{
     global $routes;
     foreach ($routes as $domain => $route) {
         if ($route['id'] == $id) {
@@ -16,36 +17,43 @@ function getDomainFor(string $id) {
     }
 }
 
+$relationships = json_decode(base64_decode($_ENV['PLATFORM_RELATIONSHIPS']), true);
+
+if (empty($relationships)) {
+    throw new \Exception("PLATFORM_RELATIONSHIPS Not Set or empty.");
+}
+
+$redis = $relationships['redis'][0];
+$redis['keyPrefix'] = 'live-';
 $variables = json_decode(base64_decode($_ENV['PLATFORM_VARIABLES']), true);
 
 if (empty($variables)) {
     throw new \Exception("PLATFORM_VARIABLES Not Set Or Empty.");
 }
 
-$config = array(
-    'slim' => array(
-        'mode' => 'live',
-        'debug' => 0,
-        'cookies' => array(
+$config = [
+    'slim' => [
+        'mode'     => 'live',
+        'debug'    => 0,
+        'cookies'  => [
             'secret_key' => $variables['web.config.slim.cookies.secret_key'],
-        ),
-        'custom' => array(
-            'redisKeyPrefix' => 'live-',
-            'apiUrl' => getDomainFor("api"),
+        ],
+        'custom'   => [
+            'redis'             => $redis,
+            'apiUrl'            => getDomainFor("api"),
             'googleAnalyticsId' => $variables['web.config.slim.custom.googleAnalyticsId'],
-            'csrfSecret' => $variables['web.config.slim.custom.csrfSecret']
+            'csrfSecret'        => $variables['web.config.slim.custom.csrfSecret']
             //'useMinifiedFiles' => true
-        ),
-        'oauth' => array(
-            'client_id' => $variables['web.config.slim.oauth.client_id'],
+        ],
+        'oauth'    => [
+            'client_id'     => $variables['web.config.slim.oauth.client_id'],
             'client_secret' => $variables['web.config.slim.oauth.client_secret'],
-        ),
-        'twig' => array(
-            'cache' => $variables['web.config.slim.twig.cache']
-        ),
-        'facebook' => array(
+        ],
+        'twig'     => [
+            'cache' => $variables['web.config.slim.twig.cache'],
+        ],
+        'facebook' => [
             'app_id' => $variables['web.config.slim.facebook.app_id'],
-        ),
-    ),
-);
-
+        ],
+    ],
+];
