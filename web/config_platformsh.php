a92737d136c73cfa89a78b7cdb3745fc23fb45a6
Jos Elstgeest <zawadi@gmail.com>
2019-02-28 07:44:48 +0100

    add environment based config to web

---
+++ web/config_platformsh.php
@@ -0,0 +1,34 @@
+<?php
+
+$variables = json_decode(base64_decode($_ENV['PLATFORM_VARIABLES']), true);
+
+if (empty($variables)) {
+    throw new \Exception("PLATFORM_VARIABLES Not Set Or Empty.");
+}
+
+$config = array(
+    'slim' => array(
+        'mode' => 'live',
+        'debug' => 0,
+        'cookies' => array(
+            'secret_key' => $variables['web.config.slim.cookies.secret_key'],
+        ),
+        'custom' => array(
+            'redisKeyPrefix' => 'live-',
+            'apiUrl' => $variables['web.config.slim.custom.apiUrl'],
+            'googleAnalyticsId' => $variables['web.config.slim.custom.googleAnalyticsId'],
+            'csrfSecret' => $variables['web.config.slim.custom.csrfSecret']
+            //'useMinifiedFiles' => true
+        ),
+        'oauth' => array(
+            'client_id' => $variables['web.config.slim.oauth.client_id'],
+            'client_secret' => $variables['web.config.slim.oauth.client_secret'],
+        ),
+        'twig' => array(
+            'cache' => $variables['web.config.slim.twig.cache']
+        ),
+        'facebook' => array(
+            'app_id' => $variables['web.config.slim.facebook.app_id'],
+        ),
+    ),
+);
